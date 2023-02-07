89af9fe66f91695db7057760eecdd1ac29b7eda7
Dominos0321 <dsch792@gmail.com>
2023-02-07 08:45:25 +0100

    3. jest gita le jeszcze nie

--- app/src/main/java/com/ft/jajuzniewiemczydzialajacaaplikacja/MainActivity.kt
+++ app/src/main/java/com/ft/jajuzniewiemczydzialajacaaplikacja/MainActivity.kt
@@ -2,7 +2,9 @@ package com.ft.jajuzniewiemczydzialajacaaplikacja
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.SeekBar
 import android.widget.Toast
 
@@ -14,17 +16,39 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         seekBar1 = findViewById(R.id.seekBar)
-        seekBar2 = findViewById(R.id.seekBar)
-        seekBar3 = findViewById(R.id.seekBar)
+        seekBar2 = findViewById(R.id.seekBar2)
+        seekBar3 = findViewById(R.id.seekBar3)
+        val btn = findViewById<Button>(R.id.button)
         val obraz1 = findViewById<ImageView>(R.id.imageView)
         val obraz2 = findViewById<ImageView>(R.id.imageView2)
         val obraz3 = findViewById<ImageView>(R.id.imageView3)
+        var pomocnicza : Float = "0.0".toFloat()
 
         seekBar1?.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(
                 seekBar: SeekBar, progress: Int,
                 fromUser: Boolean
             ) {
+                pomocnicza = progress.toFloat() / 100;
+                pomocnicza.toFloat()
+                obraz1.scaleX = pomocnicza.toFloat() + 0.1.toFloat()
+                obraz1.scaleY = pomocnicza.toFloat() + 0.1.toFloat()
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+            }
+        })
+        seekBar2?.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(
+                seekBar: SeekBar, progress: Int,
+                fromUser: Boolean
+            ) {
+                pomocnicza = progress.toFloat() / 100;
+                pomocnicza.toFloat()
+                obraz2.scaleX = pomocnicza.toFloat() + 0.1.toFloat()
+                obraz2.scaleY = pomocnicza.toFloat() + 0.1.toFloat()
 
             }
             override fun onStartTrackingTouch(seekBar: SeekBar) {
@@ -33,5 +57,35 @@ class MainActivity : AppCompatActivity() {
             override fun onStopTrackingTouch(seekBar: SeekBar) {
             }
         })
+        seekBar3?.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(
+                seekBar: SeekBar, progress: Int,
+                fromUser: Boolean
+            ) {
+                pomocnicza = progress.toFloat() / 100;
+                pomocnicza.toFloat()
+                obraz3.scaleX = pomocnicza.toFloat() + 0.1.toFloat()
+                obraz3.scaleY = pomocnicza.toFloat() + 0.1.toFloat()
+
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+            }
+        })
+
+    btn.setOnClickListener{
+        seekBar1?.progress = 0
+        seekBar2?.progress = 0
+        seekBar3?.progress = 0
+        obraz1.scaleX = 1.toFloat()
+        obraz1.scaleY = 1.toFloat()
+        obraz2.scaleX = 1.toFloat()
+        obraz2.scaleY = 1.toFloat()
+        obraz3.scaleX = 1.toFloat()
+        obraz3.scaleY = 1.toFloat()
+        }
     }
+
 }
