ad76318324b27d50d623c4b4ce28476b37854cf8
Dominos0321 <dsch792@gmail.com>
2023-02-07 07:53:58 +0100

    2. wszystko zadelkarowanie

--- app/src/main/java/com/ft/jajuzniewiemczydzialajacaaplikacja/MainActivity.kt
+++ app/src/main/java/com/ft/jajuzniewiemczydzialajacaaplikacja/MainActivity.kt
@@ -2,37 +2,35 @@ package com.ft.jajuzniewiemczydzialajacaaplikacja
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.SeekBar
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-    var seekBarNormal: SeekBar? = null
-    var seekBarDiscrete: SeekBar? = null
+    var seekBar1: SeekBar? = null
+    var seekBar2: SeekBar? = null
+    var seekBar3: SeekBar? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        seekBarNormal = findViewById(R.id.seekBar)
+        seekBar1 = findViewById(R.id.seekBar)
+        seekBar2 = findViewById(R.id.seekBar)
+        seekBar3 = findViewById(R.id.seekBar)
+        val obraz1 = findViewById<ImageView>(R.id.imageView)
+        val obraz2 = findViewById<ImageView>(R.id.imageView2)
+        val obraz3 = findViewById<ImageView>(R.id.imageView3)
 
-        seekBarNormal?.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+        seekBar1?.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(
                 seekBar: SeekBar, progress: Int,
                 fromUser: Boolean
             ) {
-                Toast.makeText(
-                    applicationContext,
-                    "seekbar progress: $progress",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
 
+            }
             override fun onStartTrackingTouch(seekBar: SeekBar) {
-                Toast.makeText(applicationContext, "seekbar touch started!", Toast.LENGTH_SHORT)
-                    .show()
             }
 
             override fun onStopTrackingTouch(seekBar: SeekBar) {
-                Toast.makeText(applicationContext, "seekbar touch stopped!", Toast.LENGTH_SHORT)
-                    .show()
             }
         })
     }
