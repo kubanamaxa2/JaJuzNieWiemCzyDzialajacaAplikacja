bef19167fe61baf58a62cb7532bbd3e2afc68bcc
Dominos0321 <dsch792@gmail.com>
2023-02-07 07:23:15 +0100

    Initial commit

---
+++ app/src/main/java/com/ft/jajuzniewiemczydzialajacaaplikacja/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ft.jajuzniewiemczydzialajacaaplikacja
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
