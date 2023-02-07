ecb722d7950112c16dd0a38d942214669b45be96
Dominos0321 <dsch792@gmail.com>
2023-02-07 07:41:36 +0100

    piewsze comuchowanie

--- app/src/main/java/com/ft/jajuzniewiemczydzialajacaaplikacja/MainActivity.kt
+++ app/src/main/java/com/ft/jajuzniewiemczydzialajacaaplikacja/MainActivity.kt
@@ -2,10 +2,38 @@ package com.ft.jajuzniewiemczydzialajacaaplikacja
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    var seekBarNormal: SeekBar? = null
+    var seekBarDiscrete: SeekBar? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        seekBarNormal = findViewById(R.id.seekBar)
+
+        seekBarNormal?.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(
+                seekBar: SeekBar, progress: Int,
+                fromUser: Boolean
+            ) {
+                Toast.makeText(
+                    applicationContext,
+                    "seekbar progress: $progress",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+                Toast.makeText(applicationContext, "seekbar touch started!", Toast.LENGTH_SHORT)
+                    .show()
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                Toast.makeText(applicationContext, "seekbar touch stopped!", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        })
     }
 }
