74776d5bd3a14531734ce70d5dad0bddee258928
kubanamaxa2 <jakubniemczyk09@gmail.com>
2023-02-13 19:29:19 +0100

    nie wiem który komit ale jest czwóreczka

--- app/src/main/java/com/ft/jajuzniewiemczydzialajacaaplikacja/MainActivity.kt
+++ app/src/main/java/com/ft/jajuzniewiemczydzialajacaaplikacja/MainActivity.kt
@@ -12,12 +12,18 @@ class MainActivity : AppCompatActivity() {
     var seekBar1: SeekBar? = null
     var seekBar2: SeekBar? = null
     var seekBar3: SeekBar? = null
+    var seekBar4: SeekBar? = null
+    var seekBar5: SeekBar? = null
+    var seekBar6: SeekBar? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         seekBar1 = findViewById(R.id.seekBar)
         seekBar2 = findViewById(R.id.seekBar2)
         seekBar3 = findViewById(R.id.seekBar3)
+        seekBar4 = findViewById(R.id.seekBar4)
+        seekBar5 = findViewById(R.id.seekBar5)
+        seekBar6 = findViewById(R.id.seekBar6)
         val btn = findViewById<Button>(R.id.button)
         val obraz1 = findViewById<ImageView>(R.id.imageView)
         val obraz2 = findViewById<ImageView>(R.id.imageView2)
@@ -32,7 +38,7 @@ class MainActivity : AppCompatActivity() {
                 pomocnicza = progress.toFloat() / 100;
                 pomocnicza.toFloat()
                 obraz1.scaleX = pomocnicza.toFloat() + 0.1.toFloat()
-                obraz1.scaleY = pomocnicza.toFloat() + 0.1.toFloat()
+
             }
             override fun onStartTrackingTouch(seekBar: SeekBar) {
             }
@@ -48,7 +54,7 @@ class MainActivity : AppCompatActivity() {
                 pomocnicza = progress.toFloat() / 100;
                 pomocnicza.toFloat()
                 obraz2.scaleX = pomocnicza.toFloat() + 0.1.toFloat()
-                obraz2.scaleY = pomocnicza.toFloat() + 0.1.toFloat()
+
 
             }
             override fun onStartTrackingTouch(seekBar: SeekBar) {
@@ -65,6 +71,54 @@ class MainActivity : AppCompatActivity() {
                 pomocnicza = progress.toFloat() / 100;
                 pomocnicza.toFloat()
                 obraz3.scaleX = pomocnicza.toFloat() + 0.1.toFloat()
+
+
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+            }
+        })
+        seekBar4?.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(
+                seekBar: SeekBar, progress: Int,
+                fromUser: Boolean
+            ) {
+                pomocnicza = progress.toFloat() / 100;
+                pomocnicza.toFloat()
+                obraz1.scaleY = pomocnicza.toFloat() + 0.1.toFloat()
+
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+            }
+        })
+        seekBar5?.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(
+                seekBar: SeekBar, progress: Int,
+                fromUser: Boolean
+            ) {
+                pomocnicza = progress.toFloat() / 100;
+                pomocnicza.toFloat()
+                obraz2.scaleY = pomocnicza.toFloat() + 0.1.toFloat()
+
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+            }
+        })
+        seekBar6?.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(
+                seekBar: SeekBar, progress: Int,
+                fromUser: Boolean
+            ) {
+                pomocnicza = progress.toFloat() / 100;
+                pomocnicza.toFloat()
                 obraz3.scaleY = pomocnicza.toFloat() + 0.1.toFloat()
 
             }
@@ -79,6 +133,9 @@ class MainActivity : AppCompatActivity() {
         seekBar1?.progress = 0
         seekBar2?.progress = 0
         seekBar3?.progress = 0
+        seekBar4?.progress = 0
+        seekBar5?.progress = 0
+        seekBar6?.progress = 0
         obraz1.scaleX = 1.toFloat()
         obraz1.scaleY = 1.toFloat()
         obraz2.scaleX = 1.toFloat()
